69a99ca75a3e5215d9f9f0c4028c25e06f4f9dbf
NazarStf <hworker98@gmail.com>
2023-07-21 23:49:07 +0300

    Initial Commit

---
+++ HP Trivia/HP_TriviaApp.swift
@@ -0,0 +1,17 @@
+//
+//  HP_TriviaApp.swift
+//  HP Trivia
+//
+//  Created by NazarStf on 21.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct HP_TriviaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
