52e6af98a51d00767f0318280b8b0de4d5b847d7
NazarStf <hworker98@gmail.com>
2023-07-22 22:23:13 +0300

    Lesson 4 - Animating Transitions

--- HP Trivia/ContentView.swift
+++ HP Trivia/ContentView.swift
@@ -16,6 +16,7 @@ struct ContentView: View {
 	@State private var moveBackgroundImage = false
 	@State private var isMuted = false // State to track if the audio is muted.
 	@Environment(\.colorScheme) var colorScheme // determine the current color scheme.
+	@State private var animateViewsIn = false
 
 	var body: some View {
 		// Use GeometryReader to adapt the layout to the available space.
@@ -38,44 +39,55 @@ struct ContentView: View {
 				VStack {
 					// Game title and bolt icon.
 					ZStack {
-						HStack {
-							Spacer()
-							
-							// Here we insert the mute/unmute button.
-							Button(action: {
-								// Toggle the isMuted state.
-								isMuted.toggle()
-								// If the audio is to be muted, pause the audio player. Otherwise, play the audio.
-								if isMuted {
-									audioPlayer.pause()
-								} else {
-									audioPlayer.play()
+						VStack {
+							if animateViewsIn {
+								HStack {
+									Spacer()
+									
+									// Here we insert the mute/unmute button.
+									Button(action: {
+										// Toggle the isMuted state.
+										isMuted.toggle()
+										// If the audio is to be muted, pause the audio player. Otherwise, play the audio.
+										if isMuted {
+											audioPlayer.pause()
+										} else {
+											audioPlayer.play()
+										}
+									}) {
+										// Show the "play.slash" icon when the audio is muted and the "play" icon when it's not muted.
+										Image(systemName: isMuted ? "play.slash.fill" : "play.fill")
+											.font(.largeTitle)
+											.padding()
+											.foregroundColor(colorScheme == .dark ? .white : .black) // Set color based on color scheme.
+											.shadow(radius: 2)
+									}
+									.padding(25)
+									.padding(.bottom, 70)
 								}
-							}) {
-								// Show the "play.slash" icon when the audio is muted and the "play" icon when it's not muted.
-								Image(systemName: isMuted ? "play.slash.fill" : "play.fill")
-									.font(.largeTitle)
-									.padding()
-									.foregroundColor(colorScheme == .dark ? .white : .black) // Set color based on color scheme.
-									.shadow(radius: 2)
 							}
-							.padding(25)
-							.padding(.bottom, 70)
 						}
+						.animation(.easeOut(duration: 0.7).delay(2.7), value: animateViewsIn)
 						
 						VStack {
-							Image(systemName: "bolt.fill")
-								.font(.largeTitle)
-								.imageScale(.large)
-							
-							Text("HP")
-								.font(.custom(Constants.hpFont, size: 70))
-								.padding(.bottom, -50)
-							
-							Text("Trivia")
-								.font(.custom(Constants.hpFont, size: 60))
+							if animateViewsIn {
+								VStack {
+									Image(systemName: "bolt.fill")
+										.font(.largeTitle)
+										.imageScale(.large)
+									
+									Text("HP")
+										.font(.custom(Constants.hpFont, size: 70))
+										.padding(.bottom, -50)
+									
+									Text("Trivia")
+										.font(.custom(Constants.hpFont, size: 60))
+								}
+								.padding(.top, 70)
+								.transition(.move(edge: .top))
+							}
 						}
-						.padding(.top, 70)
+						.animation(.easeOut(duration: 0.7).delay(2), value: animateViewsIn)
 					}
 					.frame(width: geo.size.width)
 					
@@ -83,19 +95,25 @@ struct ContentView: View {
 					
 					// Section for recent scores.
 					VStack {
-						Text("Recent Scores")
-							.font(.title2)
-						
-						Text("33")
-						Text("27")
-						Text("15")
-						
+						if animateViewsIn {
+							VStack {
+								Text("Recent Scores")
+									.font(.title2)
+								
+								Text("33")
+								Text("27")
+								Text("15")
+								
+							}
+							.font(.title3)
+							.padding(.horizontal) 
+							.foregroundColor(.white)
+							.background(.black.opacity(0.7))
+							.cornerRadius(15)
+							.transition(.opacity)
+						}
 					}
-					.font(.title3)
-					.padding(.horizontal)
-					.foregroundColor(.white)
-					.background(.black.opacity(0.7))
-					.cornerRadius(15)
+					.animation(.linear(duration: 1).delay(4), value: animateViewsIn)
 					
 					Spacer()
 					
@@ -103,49 +121,67 @@ struct ContentView: View {
 					HStack {
 						Spacer()
 						
-						// Info button.
-						Button {
-							// Show instructions screen.
-						} label: {
-							Image(systemName: "info.circle.fill")
-								.font(.largeTitle)
-								.foregroundColor(.white)
-								.shadow(radius: 5)
-						}
-						
-						Spacer()
-						
-						// Play button.
-						Button {
-							// Start new game.
-						} label: {
-							Text("Play")
-								.font(.largeTitle)
-								.foregroundColor(.white)
-								.padding(.vertical, 7)
-								.padding(.horizontal, 50)
-								.background(.brown)
-								.cornerRadius(10)
-								.shadow(radius: 5)
-						}
-						.scaleEffect(scalePlayButton ? 1.2 : 1)
-						.onAppear {
-							withAnimation(.easeInOut(duration: 1.3).repeatForever()) {
-								scalePlayButton.toggle()
+						VStack {
+							if animateViewsIn {
+								// Info button.
+								Button {
+									// Show instructions screen.
+								} label: {
+									Image(systemName: "info.circle.fill")
+										.font(.largeTitle)
+										.foregroundColor(.white)
+										.shadow(radius: 5)
+								}
+								.transition(.offset(x: -geo.size.width/4))
 							}
 						}
+						.animation(.easeOut(duration: 0.7).delay(2.7), value: animateViewsIn)
 						
 						Spacer()
 						
-						// Settings button.
-						Button {
-							// Show settings screen.
-						} label: {
-							Image(systemName: "gearshape.fill")
-								.font(.largeTitle)
-								.foregroundColor(.white)
-								.shadow(radius: 5)
+						VStack {
+							if animateViewsIn {
+								// Play button.
+								Button {
+									// Start new game.
+								} label: {
+									Text("Play")
+										.font(.largeTitle)
+										.foregroundColor(.white)
+										.padding(.vertical, 7)
+										.padding(.horizontal, 50)
+										.background(.brown)
+										.cornerRadius(10)
+										.shadow(radius: 5)
+								}
+								.scaleEffect(scalePlayButton ? 1.2 : 1)
+								.onAppear {
+									withAnimation(.easeInOut(duration: 1.3).repeatForever()) {
+										scalePlayButton.toggle()
+									}
+								}
+								.transition(.offset(y: geo.size.height/3))
+							}
 						}
+						.animation(.easeOut(duration: 0.7).delay(2), value: animateViewsIn)
+						
+						Spacer()
+						
+						VStack {
+							if animateViewsIn {
+								// Settings button.
+								Button {
+									// Show settings screen.
+								} label: {
+									Image(systemName: "gearshape.fill")
+										.font(.largeTitle)
+										.foregroundColor(.white)
+										.shadow(radius: 5)
+								}
+								.transition(.offset(x: geo.size.width/4))
+							}
+						}
+						.animation(.easeOut(duration: 0.7).delay(2.7), value: animateViewsIn)
 						
 						Spacer()
 					}
@@ -158,6 +194,7 @@ struct ContentView: View {
 		}
 		.ignoresSafeArea()
 		.onAppear {
+			animateViewsIn = true // When you launch the app, the logo animation is played
 			// Call function to start playing audio.
 			playAudio()
 		}
@@ -184,6 +221,8 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+		VStack {
+			ContentView()
+		}
     }
 }
