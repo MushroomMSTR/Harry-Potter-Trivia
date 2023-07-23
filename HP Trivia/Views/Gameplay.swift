fc619c490bf316729b22b56e8b1cb4f9c8004843
NazarStf <hworker98@gmail.com>
2023-07-23 21:06:07 +0300

    Lesson 10 - Celebration Screen

--- HP Trivia/Views/Gameplay.swift
+++ HP Trivia/Views/Gameplay.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct Gameplay: View {
 	
 	@State private var animateViewsIn = false
+	@State private var tappedCorrectAnswer = false
 	
     var body: some View {
 		GeometryReader { geo in
@@ -112,12 +113,73 @@ struct Gameplay: View {
 				}
 				.frame(width: geo.size.width, height: geo.size.height)
 				.foregroundColor(.white)
+				
+				// MARK: Selebration
+				VStack {
+					
+					Spacer()
+					
+					VStack {
+						if tappedCorrectAnswer {
+							Text("5")
+								.font(.largeTitle)
+								.padding(.top, 50)
+								.transition(.offset(y: -geo.size.height/4))
+						}
+					}
+					.animation(.easeInOut(duration: 1).delay(2), value: tappedCorrectAnswer)
+					
+					Spacer()
+					
+					VStack {
+						if tappedCorrectAnswer {
+							Text("Brilliant!")
+								.font(.custom(Constants.hpFont, size: 100))
+								.transition(.scale.combined(with: .offset(y: -geo.size.height/2)))
+						}
+					}
+					.animation(.easeInOut(duration: 1).delay(1), value: tappedCorrectAnswer)
+					
+					Spacer()
+					
+					Text("Answer 1")
+						.minimumScaleFactor(0.5)
+						.multilineTextAlignment(.center)
+						.padding(10)
+						.frame(width: geo.size.width/2.15, height: 80)
+						.background(.green.opacity(0.5))
+						.cornerRadius(25)
+						.scaleEffect(2)
+					
+					Spacer()
+					Spacer()
+					
+					Group {
+						VStack {
+							if tappedCorrectAnswer {
+								Button("Next Level >") {
+									// TODO: Reset Level for next question
+								}
+								.buttonStyle(.borderedProminent)
+								.tint(.blue.opacity(0.5))
+								.font(.largeTitle)
+								.transition(.offset(y: geo.size.height/3))
+							}
+						}
+						.animation(.easeInOut(duration: 2.7).delay(2.7), value: tappedCorrectAnswer)
+						
+						Spacer()
+						Spacer()
+					}
+				}
+				.foregroundColor(.white)
 			}
 			.frame(width: geo.size.width, height: geo.size.height)
 		}
 		.ignoresSafeArea()
 		.onAppear {
 			animateViewsIn = true
+			tappedCorrectAnswer = true
 		}
 	}
 }
