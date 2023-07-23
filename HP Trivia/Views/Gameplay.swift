e21d2a7e9fe1e1a34f7a04791b8b5177c5cdc485
NazarStf <hworker98@gmail.com>
2023-07-23 19:45:44 +0300

    Lesson 9 - Gameplay Transitions

--- HP Trivia/Views/Gameplay.swift
+++ HP Trivia/Views/Gameplay.swift
@@ -8,6 +8,9 @@
 import SwiftUI
 
 struct Gameplay: View {
+	
+	@State private var animateViewsIn = false
+	
     var body: some View {
 		GeometryReader { geo in
 			ZStack {
@@ -17,6 +20,7 @@ struct Gameplay: View {
 					.overlay(Rectangle().foregroundColor(.black.opacity(0.8)))
 				
 				VStack {
+					// MARK: Controls
 					HStack {
 						Button("End Game") {
 							// TODO: End game
@@ -31,48 +35,75 @@ struct Gameplay: View {
 					.padding()
 					.padding(.vertical, 30)
 					
-					Text("Who is Harry Potter?")
-						.font(.custom(Constants.hpFont, size: 50))
-						.multilineTextAlignment(.center)
-						.padding()
+					// MARK: Question
+					VStack {
+						if animateViewsIn {
+							Text("Who is Harry Potter?")
+								.font(.custom(Constants.hpFont, size: 50))
+								.multilineTextAlignment(.center)
+								.padding()
+								.transition(.scale)
+						}
+					}
+					.animation(.easeOut(duration: 2), value: animateViewsIn)
 					
 					Spacer()
 					
+					// MARK: Hints
 					HStack {
-						Image(systemName: "questionmark.app.fill")
-							.resizable()
-							.scaledToFit()
-							.frame(width: 100)
-							.foregroundColor(.cyan)
-							.rotationEffect(.degrees(-15))
-							.padding()
-							.padding(.leading, 20)
+						VStack {
+							if animateViewsIn {
+								Image(systemName: "questionmark.app.fill")
+									.resizable()
+									.scaledToFit()
+									.frame(width: 100)
+									.foregroundColor(.cyan)
+									.rotationEffect(.degrees(-15))
+									.padding()
+									.padding(.leading, 20)
+									.transition(.offset(x: -geo.size.width/2))
+							}
+						}
+						.animation(.easeOut(duration: 1.5).delay(2), value: animateViewsIn)
 						
 						Spacer()
 						
-						Image(systemName: "book.closed")
-							.resizable()
-							.scaledToFit()
-							.frame(width: 50)
-							.foregroundColor(.black)
-							.frame(width: 100, height: 100)
-							.background(.cyan)
-							.cornerRadius(20)
-							.rotationEffect(.degrees(15))
-							.padding()
-							.padding(.trailing, 20)
+						VStack {
+							if animateViewsIn {
+								Image(systemName: "book.closed")
+									.resizable()
+									.scaledToFit()
+									.frame(width: 50)
+									.foregroundColor(.black)
+									.frame(width: 100, height: 100)
+									.background(.cyan)
+									.cornerRadius(20)
+									.rotationEffect(.degrees(15))
+									.padding()
+									.padding(.trailing, 20)
+									.transition(.offset(x: geo.size.width/2))
+							}
+						}
+						.animation(.easeOut(duration: 1.5).delay(2), value: animateViewsIn)
 					}
 					.padding(.bottom)
 					
+					// MARK: Answers
 					LazyVGrid(columns: [GridItem(), GridItem()]) {
 						ForEach(1..<5) { i in
-							Text(i == 3 ? "The boy who basically leved and got sent to his relatives house where he was treated quite badly if I'm being honest but yeah." : "Aswer \(i)")
-								.minimumScaleFactor(0.5)
-								.multilineTextAlignment(.center)
-								.padding(10)
-								.frame(width: geo.size.width/2.15, height: 80)
-								.background(.green.opacity(0.5))
-								.cornerRadius(25)
+							VStack {
+								if animateViewsIn {
+									Text(i == 3 ? "The boy who basically leved and got sent to his relatives house where he was treated quite badly if I'm being honest but yeah." : "Aswer \(i)")
+										.minimumScaleFactor(0.5)
+										.multilineTextAlignment(.center)
+										.padding(10)
+										.frame(width: geo.size.width/2.15, height: 80)
+										.background(.green.opacity(0.5))
+										.cornerRadius(25)
+										.transition(.scale)
+								}
+							}
+							.animation(.easeOut(duration: 1).delay(1.5), value: animateViewsIn)
 						}
 					}
 					
@@ -85,11 +116,16 @@ struct Gameplay: View {
 			.frame(width: geo.size.width, height: geo.size.height)
 		}
 		.ignoresSafeArea()
+		.onAppear {
+			animateViewsIn = true
+		}
 	}
 }
 
 struct Gameplay_Previews: PreviewProvider {
     static var previews: some View {
-        Gameplay()
+		VStack {
+			Gameplay()
+		}
     }
 }
