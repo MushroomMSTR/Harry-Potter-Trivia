3e94c55f2d9e0efc4c60584e722a7a0141180012
NazarStf <hworker98@gmail.com>
2023-07-25 18:29:00 +0300

    Lesson 17 - In-App Purchases Pt 2: Purchase Functions

--- HP Trivia/Store.swift
+++ HP Trivia/Store.swift
@@ -18,14 +18,70 @@ enum BookStatus {
 class Store: ObservableObject {
 	@Published var books: [BookStatus] = [.active, .active, .inactive, .locked, .locked, .locked, .locked]
 	@Published var products: [Product] = []
+	@Published var purchasedIDs = Set<String>()
 	
 	private var productIDs = ["hp4", "hp5", "hp6", "hp7"]
+	private var updates: Task<Void, Never>? = nil
+	
+	init() {
+		updates = watchForUpdates()
+	}
 	
 	func loadProducts() async {
 		do {
 			products = try await Product.products(for: productIDs)
 		} catch {
 			print("Couldn't fetch those products: \(error)")
+		}
+	}
+	
+	func purchase(_ product: Product) async {
+		do {
+			let result = try await product.purchase()
+			
+			switch result {
+			case .success(let verificationResult):
+				switch verificationResult {
+				case .unverified(let signedType, let verificationError):
+					print("Error on \(signedType): \(verificationError)")
+				case .verified(let signedType):
+					purchasedIDs.insert(signedType.productID)
+				}
+			case .userCancelled:
+				break
+			case .pending:
+				break
+			@unknown default:
+				break
+			}
+		} catch {
+			print("Couldn't purchase that product: \(error)")
+		}
+	}
+	
+	private func checkPurchased() async {
+		for product in products {
+			guard let state = await product.currentEntitlement else { return }
+			
+			switch state {
+			case .unverified(let signedType, let verificationError):
+				print("Error on \(signedType): \(verificationError)")
+				
+			case .verified(let signedType):
+				if signedType.revocationDate == nil {
+					purchasedIDs.insert(signedType.productID)
+				} else {
+					purchasedIDs.remove(signedType.productID)
+				}
+			}
+		}
+	}
+	
+	private func watchForUpdates() -> Task<Void, Never> {
+		Task(priority: .background) {
+			for await _ in Transaction.updates {
+				await checkPurchased()
+			}
 		}
 	}
 }
